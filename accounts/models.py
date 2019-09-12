c11312df84c53ed5e08ac670c197e14a1dab9b2b
nimosaid <saidnimo123@gmail.com>
2019-09-12 18:45:40 +0300

    starting code

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from neighbors.models import Neighborhood
+from django.db.models.signals import post_save
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    photo = models.ImageField(upload_to = 'profiles/')
+    email = models.EmailField(blank=True)
+    neigborhood = models.ForeignKey(Neighborhood,  on_delete=models.CASCADE, related_name='area')
+    
+    def __str__(self):
+        return f'{self.user.username}'
+
+'''
+We dont have to create profiles every time a user signs up. Instead,
+we are going to use django signals to get that done automatically.
+'''
