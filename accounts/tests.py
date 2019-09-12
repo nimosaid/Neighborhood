f707a1510329660fcb3044bfa7f7f7ac33cc2997
nimosaid <saidnimo123@gmail.com>
2019-09-12 18:44:02 +0300

    starting code

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+from neighbors.models import Neighborhood
 
 # Create your tests here.
+
+
+class UserTestClass(TestCase):
+
+    def setUp(self):
+        self.user1 = User(
+            username = 'test1',
+            password =  'tehsingpass'
+        )
+        self.neigborhood = Neighborhood(name='twst',
+                                        location='testloc',
+                                        address = '1234 Road',
+                                        city = 'Nairobi')
+        self.profile1 = Profile (
+ 
+             user = self.user1,
+             photo = 'pic.jpg',
+             email = 'gabriel.oduori@gmail.com',
+             neigborhood = self.neigborhood)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user1, User))
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile1, Profile))
