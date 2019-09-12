313438676e873eb426bed4841297d3c0638f1a32
nimosaid <saidnimo123@gmail.com>
2019-09-12 18:49:19 +0300

    writing code

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','email',]
+    
+    
+    
+    
+admin.site.register(Profile, ProfileAdmin)
