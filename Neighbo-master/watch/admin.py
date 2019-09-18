3ba45cb211cb8d222c22ba8354a090bdb5a653e4
nimosaid <saidnimo123@gmail.com>
2019-09-18 13:42:39 +0300

    finishing up

--- Neighbo-master/watch/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from watch.models import *
-# Register your models here.
-
-admin.site.register(Business)
-admin.site.register(Profile)
-admin.site.register(NeighborHood)
-admin.site.register(Post)
-admin.site.register(Comment)
